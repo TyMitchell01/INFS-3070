4bf104ad60faad9f31b67e5d3fc87915fb52e0b4
MyTitchell <98676331+MyTitchell@users.noreply.github.com>
2022-02-12 21:47:53 -0700

    Correct Hw_2

--- Homework  Practice/src/hw_2.java
+++ Homework  Practice/src/hw_2.java
@@ -1,4 +1,42 @@
+import java.util.Scanner;
 
 public class hw_2 {
+
+	public static void main(String[] args) {
+	
+		final int questions = 3;					//Number of Questions
+		String[] facts = new String[questions];		//Defining Array
+		
+		//Scanner object
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Please input your information");
+		
+		//Student Name
+		System.out.println("Name:");
+		facts[0] = keyboard.nextLine();
+		
+		//Student Major
+		System.out.println("Major:");
+		facts[1] = keyboard.nextLine();
+		
+		//Student birthplace
+		System.out.println("Birthplace");
+		facts[2] = keyboard.nextLine();
+		
+		//Display student credentials
+		System.out.println("Your Student Info:");
+		System.out.println(facts[0]);
+		System.out.println(facts[1]);
+		System.out.println(facts[2]);
+		
+		keyboard.close();
+		
+		
+		//Ending math equation
+		
+		
+
+	}
 
 }
